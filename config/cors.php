ccdb5c1a56352e16bcb6ed22d37cba3710c082b1
unknown <osemensilas@gmail.com>
2026-09-12 07:30:42 +0100

    New Commit

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://affiliate.iruhost.com'],
+    'allowed_origins' => ['https://affiliate-backend.iruhost.com'],
 
     'allowed_origins_patterns' => [],
 
