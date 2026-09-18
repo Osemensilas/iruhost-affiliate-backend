fa1ee7f6f924674eaa5894d8f755eacfb2cceb83
unknown <osemensilas@gmail.com>
2026-09-18 06:40:00 +0100

    New Commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,9 +25,7 @@ class UserController extends Controller
     public function GetReferrals(Request $request){
         $userId = $request->user()->user_id;
 
-        $user = AffiliateUser::with('account')
-            ->where('user_id', $userId)
-            ->first();
+        $user = AffiliateUser::with('account')->where('user_id', $userId)->first();
 
         if (!$user) {
             return response()->json([
@@ -39,22 +37,17 @@ class UserController extends Controller
         $referralCode = $user->referral_code;
 
         // Get all users referred by this affiliate
-        $referredUserIds = User::where('referred_by', $referralCode)
-            ->pluck('user_id');
+        $referredUserIds = User::where('referred_by', $referralCode)->pluck('user_id');
 
         // Get the 5 most recent products bought by those users
-        $products = Transactions::whereIn('user_id', $referredUserIds)
-            ->latest()
-            ->limit(5)
-            ->get();
+        $products = Transactions::whereIn('user_id', $referredUserIds)->latest()->limit(5)->get();
 
-        // if ($products < 1){
-        //    return response()->json([
-        //         'status' => 'success',
-        //         'message' => 'From get referrals',
-        //         'products' => []
-        //     ]); 
-        // }
+        foreach ($products as $product) {
+            $user = User::where('user_id', $product->user_id)->first();
+
+            $product->firstname = $user?->firstname;
+            $product->lastname = $user?->lastname;
+        }
 
         return response()->json([
             'status' => 'success',
